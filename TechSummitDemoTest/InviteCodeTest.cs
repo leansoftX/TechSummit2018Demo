d2e8726f78718e4c1666cedc2be6d2bae9fd7ddc
jackyzhou <jackyzhou@lean-soft.cn>
2018-10-31 08:24:52 +0800

    add unit test project

---
+++ TechSummitDemoTest/InviteCodeTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevopslabsTest
+{
+    [TestClass]
+    public class InviteCodeTest
+    {
+        [TestMethod]
+        public void TestInviteCodeCreate()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestInviteCodeDelete()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ TechSummitDemoTest/OrderTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechSummitDemoTest
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void TestOrderCreate()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestOrderDelete()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
